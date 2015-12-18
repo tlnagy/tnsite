4edef04bdde6d464973f9b77b463af0a7f9073b6
Tamas Nagy <tamas@tamasnagy.com>
2015-12-18 12:46:16 -0500

    further refinements to build script

--- build.sh
+++ build.sh
@@ -4,6 +4,18 @@ jekyll build
 
 if [ cv.md -nt "$0" ] || [ ! -f _site/assets/tamasnagy_cv.pdf ]; then
   echo "CV changed or missing, generating PDF"
-  pandoc cv.md -o _site/assets/tamasnagy_cv.pdf
+  pandoc cv.md -o assets/tamasnagy_cv.pdf
+  cp assets/tamasnagy_cv.pdf _site/assets/tamasnagy_cv.pdf
 fi
 touch "$0"
+
+# deploy
+if [ "$1" != "--deploy" ]; then
+  exit 0
+fi
+
+cd _site/
+git add *
+git commit -m "generate site"
+git push origin master
+cd ..
